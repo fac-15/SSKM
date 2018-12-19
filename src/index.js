d184b1a80a3c9a45007ebaa2ad88c30bf85d4f4d
saksiva <41472850+saksiva@users.noreply.github.com>
2018-12-19 17:30:36 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const app = require('./app');
-const port = 8001;
+const port = process.env.PORT || 8001;
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
